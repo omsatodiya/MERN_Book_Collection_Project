1441a29e10dddd16b83c4e7c6f60fe72487afadd
Om Satodiya <156114228+omsatodiya@users.noreply.github.com>
2024-12-28 16:59:25 +0530

    Update BooksCard.jsx

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -17,14 +17,9 @@ const BooksCard = ({ books }) => {
   }
 
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
       {books.map((item) => (
-        <div
-          key={item._id}
-          className="transform transition-all duration-300 hover:-translate-y-1 hover:shadow-lg"
-        >
-          <BookSingleCard book={item} />
-        </div>
+        <BookSingleCard key={item._id} book={item} />
       ))}
     </div>
   );
